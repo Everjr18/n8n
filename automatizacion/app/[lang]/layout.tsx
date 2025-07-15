b6a046cf1ff28df93256f6eca781a26c070dd6a6
Ever Jose Rojas perez <everjosejr@hotmail.com>
2025-07-15 10:27:26 -0500

    add new favicon

--- automatizacion/app/[lang]/layout.tsx
+++ automatizacion/app/[lang]/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import LanguageSwitcher from "@/components/language-switcher";
-
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
 	title: "Automatizaciones con n8n",
@@ -21,20 +9,13 @@ export const metadata: Metadata = {
 
 export default async function LangLayout({
 	children,
-	params,
 }: {
 	children: React.ReactNode;
-	params: Promise<{ lang: string }>;
 }) {
-	const { lang } = await params; // aquí el await
 	return (
-		<html lang={lang}>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				<LanguageSwitcher />
-				{children}
-			</body>
-		</html>
+		<section>
+			<LanguageSwitcher />
+			{children}
+		</section>
 	);
 }
--- automatizacion/app/layout.tsx
+++ automatizacion/app/layout.tsx
@@ -1,11 +1,24 @@
+import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+	subsets: ["latin"],
+	variable: "--font-sans",
+});
+
+const geistMono = Geist_Mono({
+	subsets: ["latin"],
+	variable: "--font-mono",
+});
+
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="es">
-			<body>{children}</body>
+		<html lang="es" className={`${geistSans.variable} ${geistMono.variable}`}>
+			<body className="antialiased">{children}</body>
 		</html>
 	);
 }
